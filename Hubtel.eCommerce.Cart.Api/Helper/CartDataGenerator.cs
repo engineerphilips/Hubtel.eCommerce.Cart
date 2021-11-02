25eb3b9ea9560bac1f9239271b5ec6b3968364e4
phil <engineerphilips@outlook.com>
2021-11-02 12:48:01 +0000

    Add project files.

---
+++ Hubtel.eCommerce.Cart.Api/Helper/CartDataGenerator.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hubtel.eCommerce.Cart.Api.Data;
+using Hubtel.eCommerce.Cart.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hubtel.eCommerce.Cart.Api.Helper
+{
+    public class CartDataGenerator
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using var context = new Data.ECommerceDbContext(
+                serviceProvider.GetRequiredService<DbContextOptions<Data.ECommerceDbContext>>());
+            //if (context.Customers.Any())
+            //{
+            //    return; 
+            //}
+
+            if (context.Items.Any())
+            {
+                return;
+            }
+
+            context.Items.AddRange(
+                new Models.Cart
+                {
+                    ItemId = 1,
+                    ItemName = "Milo",
+                    Quantity = 1,
+                    UnitPrice = 16.5m,
+                    Time = DateTime.UtcNow,
+                    PhoneNumber = "0555966747"
+                },
+                new Models.Cart
+                {
+                    ItemId = 2,
+                    ItemName = "Sugar",
+                    Quantity = 1,
+                    UnitPrice = 16.5m,
+                    Time = DateTime.UtcNow,
+                    PhoneNumber = "0265995210"
+                },
+                new Models.Cart
+                {
+                    ItemId = 3,
+                    ItemName = "Ideal Milk",
+                    Quantity = 1,
+                    UnitPrice = 16.5m,
+                    Time = DateTime.UtcNow,
+                    PhoneNumber = "0265995210"
+                },
+                new Models.Cart
+                {
+                    ItemId = 4,
+                    ItemName = "Pepsodent",
+                    Quantity = 1,
+                    UnitPrice = 16.5m,
+                    Time = DateTime.UtcNow,
+                    PhoneNumber = "0555966747"
+                },
+                new Models.Cart
+                {
+                    ItemId = 5,
+                    ItemName = "Insecticide",
+                    Quantity = 1,
+                    UnitPrice = 16.5m,
+                    Time = DateTime.UtcNow,
+                    PhoneNumber = "0322005300"
+                },
+                new Models.Cart
+                {
+                    ItemId = 6,
+                    ItemName = "Oil",
+                    Quantity = 1,
+                    UnitPrice = 16.5m,
+                    Time = DateTime.UtcNow,
+                    PhoneNumber = "0322005300"
+                });
+
+            //context.Customers.AddRange(new Customer()
+            //{
+            //    PhoneNumber = "0265995210", CartItems = new List<Models.Cart>()
+            //    {
+            //        new Models.Cart
+            //        {
+            //            ItemId = 1,
+            //            ItemName = "Milo",
+            //            Quantity = 1,
+            //            UnitPrice = 16.5m,
+            //            Time = DateTime.UtcNow
+            //        },
+            //        new Models.Cart
+            //        {
+            //            ItemId = 2,
+            //            ItemName = "Sugar",
+            //            Quantity = 2,
+            //            UnitPrice = 10m,
+            //            Time = DateTime.UtcNow
+            //        }
+            //    }
+            //}, new Customer()
+            //{
+            //    PhoneNumber = "0555966747",
+            //    CartItems = new List<Models.Cart>()
+            //    {
+            //        new Models.Cart
+            //        {
+            //            ItemId = 3,
+            //            ItemName = "Ideal Milk",
+            //            Quantity = 1,
+            //            UnitPrice = 16.5m,
+            //            Time = DateTime.UtcNow
+            //        },
+            //        new Models.Cart
+            //        {
+            //            ItemId = 4,
+            //            ItemName = "Pepsodent",
+            //            Quantity = 1,
+            //            UnitPrice = 16.5m,
+            //            Time = DateTime.UtcNow
+            //        },
+            //        new Models.Cart
+            //        {
+            //            ItemId = 5,
+            //            ItemName = "Insecticide",
+            //            Quantity = 1,
+            //            UnitPrice = 16.5m,
+            //            Time = DateTime.UtcNow
+            //        }
+            //    }
+            //}, new Customer()
+            //{
+            //    PhoneNumber = "0322005300",
+            //    CartItems = new List<Models.Cart>()
+            //    {
+            //        new Models.Cart
+            //        {
+            //            ItemId = 4,
+            //            ItemName = "Pepsodent",
+            //            Quantity = 1,
+            //            UnitPrice = 8m,
+            //            Time = DateTime.UtcNow
+            //        },
+            //        new Models.Cart
+            //        {
+            //            ItemId = 5,
+            //            ItemName = "Insecticide",
+            //            Quantity = 1,
+            //            UnitPrice = 15m,
+            //            Time = DateTime.UtcNow
+            //        },
+            //        new Models.Cart
+            //        {
+            //            ItemId = 6,
+            //            ItemName = "Oil",
+            //            Quantity = 3,
+            //            UnitPrice = 20,
+            //            Time = DateTime.UtcNow
+            //        }
+            //    }
+            //});
+
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Hubtel.eCommerce.Cart.Api/Startup.cs
@@ -0,0 +1,80 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hubtel.eCommerce.Cart.Api.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+namespace Hubtel.eCommerce.Cart.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase());
+            services.AddDbContext<ECommerceDbContext>(opt => opt.UseInMemoryDatabase("ECommerceDb"));
+
+            services.AddControllers();
+            // Register the Swagger Generator service. This service is responsible for genrating Swagger Documents.
+            // Note: Add this service at the end after AddMvc() or AddMvcCore().
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Hubtel eCommerce Cart API",
+                    Version = "v1"
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+            
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Cart API V1");
+
+                // To serve SwaggerUI at application's root page, set the RoutePrefix property to an empty string.
+                c.RoutePrefix = string.Empty;
+            });
+        }
+    }
+}
